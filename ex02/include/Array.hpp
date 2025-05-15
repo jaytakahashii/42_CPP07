ffd53c426c32addb721e68cc120d0e2c27b88b4d
jtakahas <jtakahas@student.42tokyo.jp>
2025-05-15 16:11:53 +0900

    fix(SRC): 👾  edit error

--- ex02/include/Array.hpp
+++ ex02/include/Array.hpp
@@ -1,22 +1,23 @@
 #pragma once
 
-#include <stdexcept>
-
 template <typename T>
 class Array {
+  // 宣言だけを書く
+ public:
+  Array();
+  Array(unsigned int n);
+  Array(const Array& other);
+  Array& operator=(const Array& other);
+  ~Array();
+
+  T& operator[](unsigned int index);
+  const T& operator[](unsigned int index) const;
+
+  unsigned int size() const;
+
  private:
   T* _array;
   unsigned int _size;
-
- public:
-  Array();                               // デフォルトコンストラクタ
-  Array(unsigned int n);                 // サイズ指定コンストラクタ
-  Array(const Array& other);             // コピーコンストラクタ
-  Array& operator=(const Array& other);  // 代入演算子
-  ~Array();                              // デストラクタ
-
-  T& operator[](unsigned int index);              // 添字演算子（読み書き）
-  const T& operator[](unsigned int index) const;  // 添字演算子（const）
-
-  unsigned int size() const;  // 要素数取得
 };
+
+#include "Array.tpp"
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,7 +1,6 @@
+#include <Array.hpp>
 #include <cstdlib>
 #include <iostream>
-
-#include "Array.hpp"
 
 #define MAX_VAL 750
 int main(int, char**) {
