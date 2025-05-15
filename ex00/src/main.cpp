32dc8c2fb1e3f2efed24d2bf9a45f240655a6ecb
jtakahas <jtakahas@student.42tokyo.jp>
2025-05-15 15:10:56 +0900

    feat(SRC): ✨  edit test

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
 #include <string>
 
+#include "color.hpp"
 #include "whatever.hpp"
 
 int main(void) {
+  std::cout << YELLOW << "=== TEST 1 ===" << RESET << std::endl;
   int a = 2;
   int b = 3;
-  ::swap(a, b);
-  std::cout << "a = " << a << ", b = " << b << std::endl;
-  std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
-  std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+  std::cout << BOLDWHITE << "Before swap: " << RESET << "a = " << a
+            << ", b = " << b << std::endl;
 
-  std::string c = "chaine1";
-  std::string d = "chaine2";
+  ::swap(a, b);
+  std::cout << BOLDWHITE << "After swap: " << RESET << "a = " << a
+            << ", b = " << b << std::endl;
+
+  std::cout << std::endl;
+
+  std::cout << BOLDWHITE "min(a, b) = " RESET << ::min(a, b) << std::endl;
+  std::cout << BOLDWHITE "max(a, b) = " RESET << ::max(a, b) << std::endl;
+
+  std::cout << YELLOW << "\n=== TEST 2 ===" << RESET << std::endl;
+
+  std::string c = "42tokyo";
+  std::string d = "24tokyo";
+  std::cout << BOLDWHITE << "Before swap: " << RESET << "c = " << c
+            << ", d = " << d << std::endl;
+
   ::swap(c, d);
-  std::cout << "c = " << c << ", d = " << d << std::endl;
-  std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
-  std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+  std::cout << BOLDWHITE << "After swap: " << RESET << "c = " << c
+            << ", d = " << d << std::endl;
+  std::cout << std::endl;
+  std::cout << BOLDWHITE << "min(c, d) = " RESET << ::min(c, d) << std::endl;
+  std::cout << BOLDWHITE << "max(c, d) = " RESET << ::max(c, d) << std::endl;
 
   return 0;
 }
